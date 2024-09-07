887aec20b8be582a9a1167619212a5f4aaaa3b9c
Efonovia <lordnovia@gmail.com>
2024-09-07 17:09:31 +0100

    cloudinary config fix

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ dotenv.config()
 const app = express()
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
   // Multer storage configuration with dynamic `public_id`
